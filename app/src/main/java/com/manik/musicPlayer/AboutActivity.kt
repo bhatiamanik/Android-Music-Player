177b7ebd77c55254ae9229a6d5205c223e5db915
Manik <manbhatia007@gmail.com>
2024-07-29 23:21:21 +0530

    init

---
+++ app/src/main/java/com/manik/musicPlayer/AboutActivity.kt
@@ -0,0 +1,23 @@
+package com.manik.musicPlayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.manik.musicPlayer.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(MainActivity.currentThemeNav[MainActivity.themeIndex])
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About"
+        binding.aboutText.text = aboutText()
+    }
+    private fun aboutText(): String{
+        return "Developed By: Manik" +
+                "\n\nIf you want to provide feedback, I will love to hear that."
+    }
+}
